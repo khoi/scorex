71e229ce490025a74ac2fa9144eb9721288d92c9
khoi <git@khoi.io>
2021-01-27 23:14:03 +0700

    better UI

--- src/Match.js
+++ src/Match.js
@@ -89,10 +89,12 @@ class Match extends Component {
 
       if (!this.state.gameId) {
         // no game id redirect to the most feasible game
-        let possibleGames = res.data.event.match.games.filter(g => g.state !== "unneeded");
+        let possibleGames = res.data.event.match.games.filter(
+          (g) => g.state !== "unneeded"
+        );
         let g =
-            possibleGames.find((a) => a.state === "inProgress") ||
-            possibleGames[possibleGames.length - 1];
+          possibleGames.find((a) => a.state === "inProgress") ||
+          possibleGames[possibleGames.length - 1];
         window.location.href = `/match/${this.state.eventId}/${g.id}`;
         return;
       }
@@ -143,11 +145,9 @@ class Match extends Component {
 
   render() {
     console.log(this.state);
-    if (!this.state.event || !this.state.window) {
-      return <div>Loading</div>;
-    }
-    return (
-      <div className="App">
+
+    let header = this.state.event ? (
+      <>
         <div className="overview-pane">
           <div className="event-header">
             <div className="EventMatchScore">
@@ -165,7 +165,10 @@ class Match extends Component {
                       />
                     </div>
                   </div>
-                  <div className="separator">{this.state.event.match.teams[0].result.gameWins} - {this.state.event.match.teams[1].result.gameWins}</div>
+                  <div className="separator">
+                    {this.state.event.match.teams[0].result.gameWins} -{" "}
+                    {this.state.event.match.teams[1].result.gameWins}
+                  </div>
                   <div className="team">
                     <div className="tricode">
                       {this.state.event.match.teams[1].name}
@@ -186,177 +189,200 @@ class Match extends Component {
         <div className="VodsGameSelector active-selection">
           <div className="games">
             <span className="label">GAME</span>
-            {this.state.event.match.games.filter(g => g.state !== "unneeded").map((value, idx) => {
-              return (
-                <a
-                  key={idx}
-                  href={`/match/${this.state.eventId}/${value.id}`}
-                  className={
-                    value.id === this.state.gameId ? "game selected" : "game"
-                  }
-                >
-                  {value.number}
-                </a>
-              );
-            })}
+            {this.state.event.match.games
+              .filter((g) => g.state !== "unneeded")
+              .map((value, idx) => {
+                return (
+                  <a
+                    key={idx}
+                    href={`/match/${this.state.eventId}/${value.id}`}
+                    className={
+                      value.id === this.state.gameId ? "game selected" : "game"
+                    }
+                  >
+                    {value.number}
+                  </a>
+                );
+              })}
           </div>
         </div>
-        <div className="StatsTeams">
-          <div className="StatsTeamsSummary">
-            <div className="dragons">
-              <div className="blue-team">
-                {getLastFrame(this.state.window).blueTeam.dragons.map(
-                  (d, i) => {
-                    return <div key={i} className={"dragon " + d} />;
-                  }
-                )}
+      </>
+    ) : (
+      <div>Loading</div>
+    );
+
+    let body =
+      this.state.event && this.state.window ? (
+        <>
+          <div className="StatsTeams">
+            <div className="StatsTeamsSummary">
+              <div className="dragons">
+                <div className="blue-team">
+                  {getLastFrame(this.state.window).blueTeam.dragons.map(
+                    (d, i) => {
+                      return <div key={i} className={"dragon " + d} />;
+                    }
+                  )}
+                </div>
+                <div className="title">DRAGONS</div>
+                <div className="red-team">
+                  {getLastFrame(this.state.window).redTeam.dragons.map(
+                    (d, i) => {
+                      return <div key={i} className={"dragon " + d} />;
+                    }
+                  )}
+                </div>
               </div>
-              <div className="title">DRAGONS</div>
+              <div className="gold">
+                <div className="bar">
+                  <div
+                    className="blue-team"
+                    style={{
+                      flex: `${getBlueTeamGoldPercentage(
+                        this.state.window
+                      )} 1 0%`,
+                    }}
+                  />
+                  <div
+                    className="red-team"
+                    style={{
+                      flex: `${getRedTeamGoldPercentage(
+                        this.state.window
+                      )} 1 0%`,
+                    }}
+                  />
+                </div>
+                <div className="totals">
+                  <div className="blue-team">
+                    {getLastFrame(this.state.window).blueTeam.totalGold}
+                  </div>
+                  <div className="title">
+                    {getLastFrame(this.state.window).gameState.toUpperCase()}
+                  </div>
+                  <div className="red-team">
+                    {getLastFrame(this.state.window).redTeam.totalGold}
+                  </div>
+                </div>
+              </div>
+              <div className="details">
+                <div className="blue-team">
+                  <div className="stat inhibitors">
+                    <img alt="" src={inhibitorIcon} />
+                    {getLastFrame(this.state.window).blueTeam.inhibitors}
+                  </div>
+                  <div className="stat barons">
+                    <img alt="" src={baronIcon} />
+                    {getLastFrame(this.state.window).blueTeam.barons}
+                  </div>
+                  <div className="stat towers">
+                    <img alt="" src={towerIcon} />
+                    {getLastFrame(this.state.window).blueTeam.towers}
+                  </div>
+                  <div className="stat kills">
+                    <img alt="" src={killIcon} />
+                    {getBluePlayersStats(this.state.window).reduce(
+                      (acc, val) => acc + val.kills,
+                      0
+                    )}
+                  </div>
+                </div>
+                <div className="red-team">
+                  <div className="stat inhibitors">
+                    <img alt="" src={inhibitorIcon} />
+                    {getLastFrame(this.state.window).redTeam.inhibitors}
+                  </div>
+                  <div className="stat barons">
+                    <img alt="" src={baronIcon} />
+                    {getLastFrame(this.state.window).redTeam.barons}
+                  </div>
+                  <div className="stat towers">
+                    <img alt="" src={towerIcon} />
+                    {getLastFrame(this.state.window).redTeam.towers}
+                  </div>
+                  <div className="stat kills">
+                    <img alt="" src={killIcon} />
+                    {getRedPlayersStats(this.state.window).reduce(
+                      (acc, val) => acc + val.kills,
+                      0
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="StatsTeamsPlayers">
+              <div className="blue-team">
+                {getBluePlayers(this.state.window).map((p, i) => {
+                  return (
+                    <Player
+                      key={i}
+                      role={p.role}
+                      summonerName={p.summonerName}
+                      championId={p.championId}
+                      currentHealth={
+                        getBluePlayersStats(this.state.window)[i].currentHealth
+                      }
+                      maxHealth={
+                        getBluePlayersStats(this.state.window)[i].maxHealth
+                      }
+                      lvl={getBluePlayersStats(this.state.window)[i].level}
+                      k={getBluePlayersStats(this.state.window)[i].kills}
+                      d={getBluePlayersStats(this.state.window)[i].deaths}
+                      a={getBluePlayersStats(this.state.window)[i].assists}
+                      cs={getBluePlayersStats(this.state.window)[i].creepScore}
+                      gold={getBluePlayersStats(this.state.window)[i].totalGold}
+                    />
+                  );
+                })}
+              </div>
+              <div className="gold-diffs">
+                {[0, 1, 2, 3, 4].map((idx) => {
+                  let blueP = getBluePlayersStats(this.state.window)[idx];
+                  let redP = getRedPlayersStats(this.state.window)[idx];
+                  let diff = blueP.totalGold - redP.totalGold;
+                  return (
+                    <div key={idx} className="wrapper">
+                      <div className={"diff " + (diff > 0 ? "blue" : "red")}>
+                        {Math.abs(diff)}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
               <div className="red-team">
-                {getLastFrame(this.state.window).redTeam.dragons.map((d, i) => {
-                  return <div key={i} className={"dragon " + d} />;
+                {getRedPlayers(this.state.window).map((p, i) => {
+                  return (
+                    <Player
+                      key={i}
+                      role={p.role}
+                      summonerName={p.summonerName}
+                      championId={p.championId}
+                      currentHealth={
+                        getRedPlayersStats(this.state.window)[i].currentHealth
+                      }
+                      maxHealth={
+                        getRedPlayersStats(this.state.window)[i].maxHealth
+                      }
+                      lvl={getRedPlayersStats(this.state.window)[i].level}
+                      k={getRedPlayersStats(this.state.window)[i].kills}
+                      d={getRedPlayersStats(this.state.window)[i].deaths}
+                      a={getRedPlayersStats(this.state.window)[i].assists}
+                      cs={getRedPlayersStats(this.state.window)[i].creepScore}
+                      gold={getRedPlayersStats(this.state.window)[i].totalGold}
+                    />
+                  );
                 })}
               </div>
             </div>
-            <div className="gold">
-              <div className="bar">
-                <div
-                  className="blue-team"
-                  style={{
-                    flex: `${getBlueTeamGoldPercentage(
-                      this.state.window
-                    )} 1 0%`,
-                  }}
-                />
-                <div
-                  className="red-team"
-                  style={{
-                    flex: `${getRedTeamGoldPercentage(this.state.window)} 1 0%`,
-                  }}
-                />
-              </div>
-              <div className="totals">
-                <div className="blue-team">
-                  {getLastFrame(this.state.window).blueTeam.totalGold}
-                </div>
-                <div className="title">
-                  {getLastFrame(this.state.window).gameState.toUpperCase()}
-                </div>
-                <div className="red-team">
-                  {getLastFrame(this.state.window).redTeam.totalGold}
-                </div>
-              </div>
-            </div>
-            <div className="details">
-              <div className="blue-team">
-                <div className="stat inhibitors">
-                  <img alt="" src={inhibitorIcon} />
-                  {getLastFrame(this.state.window).blueTeam.inhibitors}
-                </div>
-                <div className="stat barons">
-                  <img alt="" src={baronIcon} />
-                  {getLastFrame(this.state.window).blueTeam.barons}
-                </div>
-                <div className="stat towers">
-                  <img alt="" src={towerIcon} />
-                  {getLastFrame(this.state.window).blueTeam.towers}
-                </div>
-                <div className="stat kills">
-                  <img alt="" src={killIcon} />
-                  {getBluePlayersStats(this.state.window).reduce(
-                    (acc, val) => acc + val.kills,
-                    0
-                  )}
-                </div>
-              </div>
-              <div className="red-team">
-                <div className="stat inhibitors">
-                  <img alt="" src={inhibitorIcon} />
-                  {getLastFrame(this.state.window).redTeam.inhibitors}
-                </div>
-                <div className="stat barons">
-                  <img alt="" src={baronIcon} />
-                  {getLastFrame(this.state.window).redTeam.barons}
-                </div>
-                <div className="stat towers">
-                  <img alt="" src={towerIcon} />
-                  {getLastFrame(this.state.window).redTeam.towers}
-                </div>
-                <div className="stat kills">
-                  <img alt="" src={killIcon} />
-                  {getRedPlayersStats(this.state.window).reduce(
-                    (acc, val) => acc + val.kills,
-                    0
-                  )}
-                </div>
-              </div>
-            </div>
           </div>
-          <div className="StatsTeamsPlayers">
-            <div className="blue-team">
-              {getBluePlayers(this.state.window).map((p, i) => {
-                return (
-                  <Player
-                    key={i}
-                    role={p.role}
-                    summonerName={p.summonerName}
-                    championId={p.championId}
-                    currentHealth={
-                      getBluePlayersStats(this.state.window)[i].currentHealth
-                    }
-                    maxHealth={
-                      getBluePlayersStats(this.state.window)[i].maxHealth
-                    }
-                    lvl={getBluePlayersStats(this.state.window)[i].level}
-                    k={getBluePlayersStats(this.state.window)[i].kills}
-                    d={getBluePlayersStats(this.state.window)[i].deaths}
-                    a={getBluePlayersStats(this.state.window)[i].assists}
-                    cs={getBluePlayersStats(this.state.window)[i].creepScore}
-                    gold={getBluePlayersStats(this.state.window)[i].totalGold}
-                  />
-                );
-              })}
-            </div>
-            <div className="gold-diffs">
-              {[0, 1, 2, 3, 4].map((idx) => {
-                let blueP = getBluePlayersStats(this.state.window)[idx];
-                let redP = getRedPlayersStats(this.state.window)[idx];
-                let diff = blueP.totalGold - redP.totalGold;
-                return (
-                  <div key={idx} className="wrapper">
-                    <div className={"diff " + (diff > 0 ? "blue" : "red")}>
-                      {Math.abs(diff)}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="red-team">
-              {getRedPlayers(this.state.window).map((p, i) => {
-                return (
-                  <Player
-                    key={i}
-                    role={p.role}
-                    summonerName={p.summonerName}
-                    championId={p.championId}
-                    currentHealth={
-                      getRedPlayersStats(this.state.window)[i].currentHealth
-                    }
-                    maxHealth={
-                      getRedPlayersStats(this.state.window)[i].maxHealth
-                    }
-                    lvl={getRedPlayersStats(this.state.window)[i].level}
-                    k={getRedPlayersStats(this.state.window)[i].kills}
-                    d={getRedPlayersStats(this.state.window)[i].deaths}
-                    a={getRedPlayersStats(this.state.window)[i].assists}
-                    cs={getRedPlayersStats(this.state.window)[i].creepScore}
-                    gold={getRedPlayersStats(this.state.window)[i].totalGold}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </div>
+        </>
+      ) : (
+        <div>Loading</div>
+      );
+
+    return (
+      <div className="App">
+        {header}
+        {body}
       </div>
     );
   }
